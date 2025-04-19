3786140a9e24f361504a79ff28081d2ea11dfbd8
Jia Lee <jialee8883@gmail.com>
2025-04-19 22:06:46 +0900

    Initial commit with source code

---
+++ src/main/java/com/jia/study_tracker/StudyTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.jia.study_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyTrackerApplication.class, args);
+	}
+
+}
