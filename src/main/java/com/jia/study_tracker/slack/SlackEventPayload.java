3f3390ef8444a4c4c7e578f04f11be16bf1a618f
Jia Lee <jialee8883@gmail.com>
2025-04-28 15:49:41 +0900

    UserServiceTest, SlackEventServiceTest 생성 - 각 서비스 클래스의 기본 기능과 슬랙 이벤트 처리 테스트 작성 - UserService의 동시성 관련 테스트는 다루지 않았음 - setter 메서드를 테스트에서 쓰지 않기 위해 DTO인 Payload 클래스에 @AllArgsConstructor 추가

--- src/main/java/com/jia/study_tracker/slack/SlackEventPayload.java
+++ src/main/java/com/jia/study_tracker/slack/SlackEventPayload.java
@@ -1,17 +1,18 @@
 package com.jia.study_tracker.slack;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+
 
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class SlackEventPayload {
     private String type;
     private String challenge;
     private Event event;
 
     @Getter
-    @NoArgsConstructor
+    @AllArgsConstructor
     public static class Event {
         private String type;
         private String user;
