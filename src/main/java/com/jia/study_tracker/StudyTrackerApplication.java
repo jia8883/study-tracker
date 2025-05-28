45eedcbca6598292cb227a2bb5badf21f0a0dc44
Jia Lee <jialee8883@gmail.com>
2025-05-28 17:57:17 +0900

    디버깅 중

--- src/main/java/com/jia/study_tracker/StudyTrackerApplication.java
+++ src/main/java/com/jia/study_tracker/StudyTrackerApplication.java
@@ -2,10 +2,12 @@ package com.jia.study_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ComponentScan(basePackages = "com.jia.study_tracker")
 public class StudyTrackerApplication {
 
 	public static void main(String[] args) {
